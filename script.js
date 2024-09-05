076af22c3808ad013fa792ea152b29fd71126a3e
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-09-05 17:09:57 +0300

    basic layout

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = []
+
+function Book(title, author, numberOfPages) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+}
+
+function addBookToLibrary() {
+
+}

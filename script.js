efb6df16d55aec66a3d945b8fd3369678681e800
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-09-09 15:23:06 +0300

    buttons in book list update main card info

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const myLibrary = [
 
 const listOfBooks = document.querySelector('[data-list-of-books]')
 
+const title = document.querySelector('[data-book-title]')
+const author = document.querySelector('[data-book-author]')
+const numberOfPages = document.querySelector('[data-book-pages]')
+
+
 myLibrary.forEach(book => {
     let li = document.createElement("li")
     let btn = document.createElement("button")
@@ -12,20 +17,13 @@ myLibrary.forEach(book => {
     btn.textContent = book.title
     listOfBooks.appendChild(li)
     li.appendChild(btn)
+    
+    btn.addEventListener('click', button => {
+        title.textContent = book.title
+        author.textContent = book.author
+        numberOfPages.textContent = book.numberOfPages
+    })
 });
-
-
-const title = document.querySelector('[data-book-title]')
-const author = document.querySelector('[data-book-author]')
-const numberOfPages = document.querySelector('[data-book-pages]')
-
-const update = document.querySelector('[data-update]')
-
-update.addEventListener('click', button => {
-    title.textContent = myLibrary[0].title
-    author.textContent = myLibrary[0].author
-    numberOfPages.textContent = myLibrary[0].numberOfPages
-})
 
 // function Book(title, author, numberOfPages) {
 //     this.title = title;

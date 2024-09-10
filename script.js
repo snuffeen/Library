a6f50f043fb8de41f4a18aadfa181774ad0e7729
snuffeen <133567875+snuffeen@users.noreply.github.com>
2024-09-10 16:41:24 +0300

    new book is added to list on submit

--- script.js
+++ script.js
@@ -3,32 +3,6 @@ const myLibrary = [
     {title : 'title2', author : 'author2', numberOfPages : 250}
 ]
 
-function Book(title, author, numberOfPages) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-}
-
-function addBookToLibrary(book) {
-    const formTitle = document.querySelector('#book-title')
-    const formAuthor = document.querySelector('#book-author')
-    const formNumberOfPages = document.querySelector('#number-of-pages')
-    
-    let newBook = new Book(formTitle.value, formAuthor.value, formNumberOfPages.value)
-
-    myLibrary.push(newBook)
-    formTitle.value = ''
-    formAuthor.value = ''
-    formNumberOfPages.value = ''
-}
-
-const submit = document.querySelector('[data-submit]')
-
-submit.addEventListener('click', event => {
-    event.preventDefault()
-    addBookToLibrary()
-})
-
 const listOfBooks = document.querySelector('[data-list-of-books]')
 
 const title = document.querySelector('[data-book-title]')
@@ -49,3 +23,45 @@ myLibrary.forEach(book => {
         numberOfPages.textContent = book.numberOfPages
     })
 });
+
+function update(book) {
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+    btn.classList.add("choose-book")
+    btn.textContent = book.title
+    listOfBooks.appendChild(li)
+    li.appendChild(btn)
+    
+    btn.addEventListener('click', button => {
+        title.textContent = book.title
+        author.textContent = book.author
+        numberOfPages.textContent = book.numberOfPages
+    })
+}
+
+function Book(title, author, numberOfPages) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+}
+
+function addBookToLibrary() {
+    const formTitle = document.querySelector('#book-title')
+    const formAuthor = document.querySelector('#book-author')
+    const formNumberOfPages = document.querySelector('#number-of-pages')
+    
+    let newBook = new Book(formTitle.value, formAuthor.value, formNumberOfPages.value)
+
+    myLibrary.push(newBook)
+    formTitle.value = ''
+    formAuthor.value = ''
+    formNumberOfPages.value = ''
+}
+
+const submit = document.querySelector('[data-submit]')
+
+submit.addEventListener('click', event => {
+    // event.preventDefault()
+    addBookToLibrary()
+    update(myLibrary[myLibrary.length-1])
+})
